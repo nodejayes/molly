b617b646911aebfd2da37fb81146bb29a034f510
Markus <m.gilg2@freenet.de>
2018-02-20 19:53:40 +0100

    WIP: execute all tests

---
+++ spec/unit/serve/server.spec.ts
@@ -0,0 +1,34 @@
+import {Molly} from './../../../src/index';
+import {assert} from 'chai';
+import 'mocha'
+
+const request = require('request-promise');
+
+describe('Server Spec', () => {
+    let server = new Molly.Serve.ExpressServer();
+
+    it('start server', async () => {
+        let msg = await server.start('localhost', 8086, 'mongodb://localhost:27017/', 'test_molly', true);
+        assert.equal(msg, 'server listen on http://localhost:8086/', 'invalid return message');
+    });
+
+    it('_filterRequest wrong method', async () => {
+        let rs = null;
+        try {
+            rs = await request({
+                method: 'GET',
+                uri: 'http://localhost:8086/read/user',
+                body: {params:{}},
+                json: true
+            });
+            assert.fail('GET request is good');
+        } catch (err) {
+            console.info(err.message);
+            assert.equal(err.message, '405 - {"data":null,"errors":"not supported method GET"}', 'error not returned');
+        }
+    });
+
+    it('stop server', () => {
+        server.stop();
+    });
+});
