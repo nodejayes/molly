2767c61c9b605d0021239d335483c22ac36e030d
Markus <m.gilg2@freenet.de>
2018-03-10 11:47:35 +0100

    add gitlab-ci for release and fix sonarqube

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './definitions/base_types';
 export * from './models/configuration/lookup';
 export * from './serve/server';
 export * from './interfaces/request_model';
-export * from './interfaces/response_model';'initFinish'
+export * from './interfaces/response_model';
 export * from './interfaces/route_invoker';
 export * from './interfaces/websocket_message';
 export * from './interfaces/collection_properties';
