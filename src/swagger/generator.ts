9373372c36c78ab4e7bbe65f669d1159aaa9f83c
Markus <markusgilg@outlook.de>
2018-03-20 21:04:59 +0100

    WIP: #14

--- src/swagger/generator.ts
+++ src/swagger/generator.ts
@@ -1,10 +1,45 @@
 import {Logic} from './../logic';
 
+interface ISwaggerDocument {
+    version: string;
+    host: string;
+    basePath: string;
+    schemes: string[];
+    info: ISwaggerInfo[];
+    tags: ISwaggerTag[];
+    paths: ISwaggerPath[];
+    definitions: ISwaggerDefinition[];
+}
+
+interface ISwaggerInfo {
+
+}
+
+interface ISwaggerPath {
+
+}
+
+interface ISwaggerTag {
+
+}
+
+interface ISwaggerDefinition {
+    
+}
+
 class DocumentationGenerator {
     constructor() {}
 
-    generate() {
+    private _readConfiguration() {
         let collections = Logic.Configuration.collectionInfos;
         let operations = Logic.Configuration.operationInfos;
+
+        for (let i = 0; i < collections.length; i++) {
+
+        }
+
+        for (let i = 0; i < operations.length; i++) {
+
+        }
     }
 }
