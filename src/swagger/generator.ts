dbdce2b8c5c620b7cf3305c50057269b085c6f25
Markus <markusgilg@outlook.de>
2018-03-20 20:56:34 +0100

    WIP: v#14

---
+++ src/swagger/generator.ts
@@ -0,0 +1,10 @@
+import {Logic} from './../logic';
+
+class DocumentationGenerator {
+    constructor() {}
+
+    generate() {
+        let collections = Logic.Configuration.collectionInfos;
+        let operations = Logic.Configuration.operationInfos;
+    }
+}
